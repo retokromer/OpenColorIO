7435d48bc98969e9b8a20e530026a6e8e20a5fec
Michael Dolan <mdolan@imageworks.com>
2019-05-31 16:06:09 -0700

    Fix env var scope issue

--- share/ci/scripts/install_sonar.sh
+++ share/ci/scripts/install_sonar.sh
@@ -10,15 +10,13 @@ cd _sonar
 wget https://sonarcloud.io/static/cpp/build-wrapper-linux-x86.zip
 unzip build-wrapper-linux-x86.zip
 mv build-wrapper-linux-x86 /var/opt/.
-
-export PATH="/var/opt/build-wrapper-linux-x86:${PATH}"
+ln -s /var/opt/build-wrapper-linux-x86/build-wrapper-linux-x86-64 /usr/bin/build-wrapper-linux-x86-64
 
 wget https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${SONAR_VERSION}-linux.zip
 unzip sonar-scanner-cli-${SONAR_VERSION}-linux.zip
 mv sonar-scanner-${SONAR_VERSION}-linux /var/opt/.
-
-export PATH="/var/opt/sonar-scanner-${SONAR_VERSION}-linux/bin:${PATH}"
-export SONAR_RUNNER_HOME="/var/opt/sonar-scanner-${SONAR_VERSION}-linux"
+ln -s /var/opt/sonar-scanner-${SONAR_VERSION}-linux/bin/sonar-scanner /usr/bin/sonar-scanner
 
 cd ..
 rm -rf _sonar
+
