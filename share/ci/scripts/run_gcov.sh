98c725907b09a1ef69d0d521611741712313804a
Michael Dolan <michdolan@gmail.com>
2019-06-02 21:56:59 -0700

    Add build machine debug to gcov script

--- share/ci/scripts/run_gcov.sh
+++ share/ci/scripts/run_gcov.sh
@@ -2,13 +2,21 @@
 
 set -ex
 
+ls .
+ls ..
+
 mkdir _coverage
 cd _coverage
+
+find ../src/ -name "*.dir" -type d
 
 for d in $(find ../src/ -name "*.dir" -type d); do
     echo "$d"
     echo "$(dirname $(dirname "$d"))/*.cpp"
     gcov -o "$d" -d "$(dirname $(dirname "$d"))/*.cpp"
 done
+
+ls .
+ls ..
 
 cd ..
