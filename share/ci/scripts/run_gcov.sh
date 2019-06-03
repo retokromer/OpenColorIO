06ce80e130c2db92e1f657662245bf8b53796b2c
Michael Dolan <michdolan@gmail.com>
2019-06-03 00:36:32 -0700

    Move gcov source search out of build dir

--- share/ci/scripts/run_gcov.sh
+++ share/ci/scripts/run_gcov.sh
@@ -5,13 +5,8 @@ set -ex
 mkdir _coverage
 cd _coverage
 
-ls ../_build
-find ../_build/ -name "*.dir" -type d
-find ../ -name "*.gcno" -type f
-find ../ -name "*.gcda" -type f
-
 for d in $(find ../_build/ -name "*.dir" -type d); do
-    gcov -o "$d" -d "$(dirname $(dirname "$d"))/*.cpp"
+    gcov -o "$d" -d "../$(echo $(dirname $(dirname "$d")) | cut -d/ -f3-)/*.cpp"
 done
 
 cd ..
