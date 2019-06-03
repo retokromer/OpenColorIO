3425a80fe2ec7722cb5cd03f69a8fa54600a87b0
Michael Dolan <michdolan@gmail.com>
2019-06-02 23:35:06 -0700

    Add gcno, gcda file search to gcov script

--- share/ci/scripts/run_gcov.sh
+++ share/ci/scripts/run_gcov.sh
@@ -7,6 +7,8 @@ cd _coverage
 
 ls ../_build
 find ../_build/ -name "*.dir" -type d
+find ../ -name "*.gcno" -type f
+find ../ -name "*.gcda" -type f
 
 for d in $(find ../_build/ -name "*.dir" -type d); do
     gcov -o "$d" -d "$(dirname $(dirname "$d"))/*.cpp"
