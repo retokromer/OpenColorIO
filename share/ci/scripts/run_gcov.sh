0fccb6222bca0ab86ebf330af90bddfc179d6f55
Michael Dolan <michdolan@gmail.com>
2019-06-03 12:35:47 -0700

    Update gcov command with regex substitutions

--- share/ci/scripts/run_gcov.sh
+++ share/ci/scripts/run_gcov.sh
@@ -5,9 +5,13 @@ set -ex
 mkdir _coverage
 cd _coverage
 
-find ../ -name "*.cpp" -exec cp -t . {} +
-find ../ -name "*.gcno" -exec cp -t . {} +
-find ../ -name "*.gcda" -exec cp -t . {} +
-find -name "*.cpp" -exec gcov -bf {} \;
+# The sed command below converts from:
+#   ../_build/src/OpenColorIO/CMakeFiles/OpenColorIO.dir/ops/Exponent.gcno
+# to:
+#   ../src/OpenColorIO/ops/Exponent.cpp
+
+for g in $(find ../_build -name "*.gcno" -type f); do
+    gcov -p -o $(dirname "$g") $(echo "$g" | sed -e 's/\/_build\//\//' -e 's/\.gcno/\.cpp/' -e 's/\/CMakeFiles.*\.dir\//\//')
+done
 
 cd ..
